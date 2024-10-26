b5465e6825ff0d1acffca41a32976a890945618b
Truc Tran <>
2024-10-27 05:24:45 +0700

    Update main code

--- src/main/java/com/livescore/LiveScoreApplication.java
+++ src/main/java/com/livescore/LiveScoreApplication.java
@@ -1,14 +1,17 @@
 package com.livescore;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@Slf4j
 public class LiveScoreApplication {
 
 	public static void main(String[] args) {
+		log.info("Start application success OK");
 		SpringApplication.run(LiveScoreApplication.class, args);
 	}
 
